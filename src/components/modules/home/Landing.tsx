6c3bd2872f6247b301344c558440d327c6bf4d10
smbmunna <smbmunna@gmail.com>
2025-04-23 00:28:53 +0600

    Data fetch in the RecentPost module

--- src/components/modules/home/Landing.tsx
+++ src/components/modules/home/Landing.tsx
@@ -1,9 +1,14 @@
 import { Input } from "@heroui/input";
+import { SearchIcon } from "../../icons";
 
 export default function Landing() {
     return (
         <div className="bg-[url('/country.jpg')] h-screen bg-cover bg-center">
-            <Input />
+            <div className="max-w-xl pt-32 mx-auto">
+                <Input 
+                startContent={<SearchIcon className="w-5 h-5 text-gray-500"/>}
+                placeholder="Search"/>
+            </div>
         </div>
     )
 }
--- src/components/modules/home/RecentPosts.tsx
+++ src/components/modules/home/RecentPosts.tsx
@@ -1,7 +1,26 @@
-export default function RecentPosts() {
+import { Button } from "@heroui/button";
+import Container from "../../UI/Container";
+import Link from "next/link";
+
+export default async function RecentPosts() {
+  const res= await fetch("http://localhost:5000/api/v1/items?sortBy=-createdAt&limit=9")
+  const data= await res.json(); 
+  console.log(data); 
+
   return (
-    <div>
-      <h1>Recent Posts</h1>
-    </div>
+    <Container>
+      <div className="my-8">
+        <h1 className="text-center">Recent Posts</h1>
+        <p className="text-center py-4">List of posted items that were recently found and reported. </p>
+      </div>
+      <div>
+        <p className="text-center py-4">Recent Posts </p>
+        <div className="flex justify-center mb-12" >
+          <Button>
+            <Link href="found-items">See All</Link>
+          </Button>
+        </div>
+      </div>
+    </Container>
   )
 }
